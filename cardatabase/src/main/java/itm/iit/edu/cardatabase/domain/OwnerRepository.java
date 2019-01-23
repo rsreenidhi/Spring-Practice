87fb3775f396773ecada3694507d7926aae11d68
Ramanuja Sreenidhi <rsreenidhi@hawk.iit.edu>
2019-01-22 18:10:55 -0800

    deleting old

--- cardatabase/src/main/java/itm/iit/edu/cardatabase/domain/OwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package itm.iit.edu.cardatabase.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface OwnerRepository extends CrudRepository<Owner, Long>  {
-
-}
--- cardatabase/src/main/java/itm/iit/edu/cardatabase/domain/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package itm.iit.edu.cardatabase.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> { 
-    User findByUsername(String username);
-}
